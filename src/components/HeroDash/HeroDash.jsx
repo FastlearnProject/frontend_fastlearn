0d7cc9b17f488d8a72eb4743b38efa9334b2585a
Agustin26l <agustin.lopez2606@gmail.com>
2024-07-07 21:52:57 -0500

    feat: Cambiar información de usuario

--- src/components/HeroDash/HeroDash.jsx
+++ src/components/HeroDash/HeroDash.jsx
@@ -1,33 +1,41 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-// Prop de los datos del usuario obtenido a través del fetch
+const URL = import.meta.env.VITE_BACKEND_URL;
 
-// Sección Hero del dashboard que trae la información del usuario
 const HeroDash = ({ userData }) => {
-  // Crear useState para seleccionar imagen y el avatar
   const [selectedImage, setSelectedImage] = useState(null);
   const [avatarUrl, setAvatarUrl] = useState("");
-
-  // Crear useState para el nombre del usuario
+  const [formData, setFormData] = useState({
+    nombre: "",
+    correo: "",
+    contrasenaPlain: "",
+    fechaNacimiento: "",
+    telefono: "",
+  });
 
   useEffect(() => {
     if (userData) {
-      // Splitear espacios en blanco
       const initials = userData.nombre
         .split(" ")
         .map((n) => n[0])
         .join("");
-        // Crear url de avatar
       const avatar = `https://ui-avatars.com/api/?name=${initials}&background=random&size=256`;
       setAvatarUrl(avatar);
+
+      setFormData({
+        nombre: userData.nombre,
+        correo: userData.correo,
+        contrasenaPlain: "",
+        fechaNacimiento: userData.fechaNacimiento,
+        telefono: userData.telefono,
+      });
     }
   }, [userData]);
 
-  // Crear evento cambio de imagenes
   const handleImageChange = (event) => {
     const file = event.target.files[0];
     if (file) {
-      // Crear nuevo lector de archivo
       const reader = new FileReader();
       reader.onloadend = () => {
         setSelectedImage(reader.result);
@@ -36,7 +44,73 @@ const HeroDash = ({ userData }) => {
     }
   };
 
-  // Si no se han extraido la información del usuario returnar en nulo
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem("token"); // Obtener el token del localStorage
+
+    if (!token) {
+      alert("Token de autenticación no encontrado");
+      return;
+    }
+
+    console.log("Datos enviados:", {
+      nombre: formData.nombre,
+      correo: formData.correo,
+      contrasenaPlain: formData.contrasenaPlain,
+      fechaNacimiento: formData.fechaNacimiento,
+      genero: userData.genero, // Asumiendo que el género no se modifica
+      telefono: formData.telefono
+    });
+
+    try {
+      const response = await axios.put(
+        `${URL}/usuario/${userData.id_usuario}`,
+        {
+          nombre: formData.nombre,
+          correo: formData.correo,
+          contrasenaPlain: formData.contrasenaPlain,
+          fechaNacimiento: formData.fechaNacimiento,
+          genero: userData.genero, // Asumiendo que el género no se modifica
+          telefono: formData.telefono
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        alert("Usuario modificado exitosamente");
+      } else {
+        alert("Error al actualizar el usuario");
+      }
+    } catch (error) {
+      console.error("Error al actualizar la información:", error);
+      if (error.response) {
+        // El servidor respondió con un estado fuera del rango 2xx
+        console.error("Datos del error:", error.response.data);
+        console.error("Estado del error:", error.response.status);
+        console.error("Encabezados del error:", error.response.headers);
+      } else if (error.request) {
+        // La solicitud fue hecha pero no hubo respuesta
+        console.error("Solicitud realizada:", error.request);
+      } else {
+        // Algo sucedió al configurar la solicitud
+        console.error("Error:", error.message);
+      }
+      alert("Error al actualizar el usuario");
+    }
+  };
+
   if (!userData) {
     return null;
   }
@@ -47,14 +121,12 @@ const HeroDash = ({ userData }) => {
         <article className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-10 my-5">
           <div className="flex flex-col items-center space-y-5">
             <div className="rounded-full md:w-72 md:h-72 flex items-center justify-center overflow-hidden">
-              {/* Concidicion si existe el avatar o la imagen seleccionada */}
               <img
                 src={selectedImage || avatarUrl}
                 alt="Avatar"
                 className="object-cover w-full h-full rounded-full"
                 id="img"
               />
-              {/* Input para subir imagen */}
               <input
                 type="file"
                 name="foto"
@@ -68,25 +140,21 @@ const HeroDash = ({ userData }) => {
               Cambiar foto
             </label>
           </div>
-          {/* Información del usuario */}
           <div className="flex flex-col space-y-2 text-md">
             <span><b>Nombre completo: </b>{userData.nombre}</span>
             <span><b>Correo: </b>{userData.correo}</span>
             <span><b>Fecha de nacimiento: </b>{userData.fechaNacimiento}</span>
             <span><b>Teléfono: </b>{userData.telefono}</span>
-            <textarea name="" id="" placeholder="Agrega una descripción sobre ti" className="p-2 outline-primary border-2 border-primary" disabled>{userData.nombre}</textarea>
           </div>
         </article>
       </section>
       <section className="flex flex-col md:flex-row justify-around mx-4 md:mx-24 my-4">
-        {/* Boton para mostrar modal de editar información */}
         <button
           className="btn w-full md:w-3/12 my-2 md:my-0 bg-primary text-white"
           onClick={() => document.getElementById("my_modal_1").showModal()}
         >
           Editar Información
         </button>
-        {/* Boton para mostrar modal de eliminar cuenta */}
         <button
           className="btn w-full md:w-3/12 my-2 md:my-0 bg-red-500 text-white"
           onClick={() => document.getElementById("my_modal_2").showModal()}
@@ -95,48 +163,65 @@ const HeroDash = ({ userData }) => {
         </button>
       </section>
 
-      {/* Modales */}
       <div className="modal">
-        {/* Editar cuenta */}
         <dialog id="my_modal_1" className="modal">
           <div className="modal-box">
             <h3 className="font-bold text-lg">Editar Información</h3>
             <p className="py-4">Presiona ESC o clic en <b>cancelar</b> para cerrar</p>
-            <form action="" className="flex flex-col space-y-5">
+            <form onSubmit={handleUpdate} className="flex flex-col space-y-5">
               <input
                 type="text"
-                placeholder={userData.nombre}
                 name="nombre"
-                value=""
+                value={formData.nombre}
+                onChange={handleChange}
                 className="px-2 py-3 outline-primary"
               />
               <input
                 type="email"
-                placeholder={userData.correo}
-                name="email"
-                value=""
+                name="correo"
+                value={formData.correo}
+                onChange={handleChange}
+                className="px-2 py-3 outline-primary"
+              />
+              <input
+                type="password"
+                name="contrasenaPlain"
+                value={formData.contrasenaPlain}
+                onChange={handleChange}
+                className="px-2 py-3 outline-primary"
+                placeholder="Nueva Contraseña"
+              />
+              <input
+                type="date"
+                name="fechaNacimiento"
+                value={formData.fechaNacimiento}
+                onChange={handleChange}
                 className="px-2 py-3 outline-primary"
               />
               <input
                 type="tel"
-                placeholder={userData.telefono}
                 name="telefono"
-                value=""
+                value={formData.telefono}
+                onChange={handleChange}
                 className="px-2 py-3 outline-primary"
               />
-              <textarea
-                placeholder="Descripción"
-                name="descripcion"
-                value=""
-                className="px-2 py-3 outline-primary"
-              ></textarea>
-              <button type="button" className="btn bg-primary hover:bg-green-500 text-white">Actualizar</button>
-              <button type="button" className="btn hover:bg-green-500 hover:text-white" onClick={() => document.getElementById("my_modal_1").close()}>Cancelar</button>
+              <button
+                type="submit"
+                className="btn bg-primary hover:bg-green-500 text-white"
+              >
+                Actualizar
+              </button>
+              <button
+                type="button"
+                className="btn hover:bg-green-500 hover:text-white"
+                onClick={() => document.getElementById("my_modal_1").close()}
+              >
+                Cancelar
+              </button>
             </form>
           </div>
         </dialog>
 
-        {/* Eliminar cuenta */}
         <dialog id="my_modal_2" className="modal">
           <div className="modal-box">
             <h3 className="font-bold text-lg text-red-700">
@@ -148,7 +233,12 @@ const HeroDash = ({ userData }) => {
             </button>
             <div className="modal-action">
               <div className="flex space-x-5">
-                <button className="btn hover:bg-green-500" onClick={() => document.getElementById("my_modal_2").close()}>Cerrar</button>
+                <button
+                  className="btn hover:bg-green-500"
+                  onClick={() => document.getElementById("my_modal_2").close()}
+                >
+                  Cerrar
+                </button>
               </div>
             </div>
           </div>
